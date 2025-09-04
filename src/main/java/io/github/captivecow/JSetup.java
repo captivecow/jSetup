e1c747c0504443734efeedbf1ba0db2f54e0f8c4
captivecow <109059183+captivecow@users.noreply.github.com>
2025-09-03 22:12:38 -0500

    feat: setup maven project, added gitignore, readme (#1)

---
+++ src/main/java/io/github/captivecow/JSetup.java
@@ -0,0 +1,10 @@
+package io.github.captivecow;
+
+/**
+ * Hello world!
+ */
+public class JSetup {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
